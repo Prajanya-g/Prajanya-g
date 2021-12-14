e9700583496a77f1422fbf4b82da4c823647ad61
Prajanya Gupta <65920698+Prajanya-g@users.noreply.github.com>
2021-12-14 11:37:05 +0530

    Delete project 1 year 1/development  directory

--- project 1 year 1/development /resize6.m
+++
@@ -1,7 +0,0 @@
-% creatign vector 
-a = [1,2,2,2,2;2,2,2,2,2;3,2,2,2,2;4,2,2,2,2;5,2,2,2,2;6,2,2,2,2;7,2,2,2,2];
-% vector for which elements to double 
-c = [2 1 2 1 2 1 2 ];
-d = [2,1,2,1,2]
-% doubling the elements
-b = repelem(a,c,d)
